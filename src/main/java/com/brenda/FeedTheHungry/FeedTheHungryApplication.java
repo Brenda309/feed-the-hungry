7d8c42f34b91b51ab2b37bbc16a73bf1e9352db7
Brenda309 <brewihogora23@gmail.com>
2025-03-24 15:13:56 +0200

    first commit

---
+++ src/main/java/com/brenda/FeedTheHungry/FeedTheHungryApplication.java
@@ -0,0 +1,13 @@
+package com.brenda.FeedTheHungry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedTheHungryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedTheHungryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brenda/FeedTheHungry/FeedTheHungryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brenda.FeedTheHungry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedTheHungryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
