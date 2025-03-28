b96eff65644b19e2668f6add7a74bd8ab1580f22
Brenda309 <brewihogora23@gmail.com>
2025-03-28 13:39:34 +0200

    Add password encoder bean.

--- src/main/java/com/brenda/FeedTheHungry/config/ApplicationConfig.java
+++ src/main/java/com/brenda/FeedTheHungry/config/ApplicationConfig.java
@@ -3,8 +3,11 @@ package com.brenda.FeedTheHungry.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.brenda.FeedTheHungry.repository.UserRepository;
 
@@ -24,6 +27,14 @@ return username -> userRepository.findByEmail(username)
 
 @Bean
 public AuthenticationProvider authenticationProvider(){
-    
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+authProvider.setUserDetailsService(userDetailsService());
+authProvider.setPasswordEncoder(PasswordEncoder());
+
+    return authProvider;
+}
+
+private PasswordEncoder PasswordEncoder() {
+    return new BCryptPasswordEncoder();
 }
 }
